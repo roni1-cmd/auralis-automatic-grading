09d364fb1037fa1946fd2efca977de492cc776ee
Ron Asnahon <asnahonron@gmail.com>
2025-06-28 12:54:54 +0800

    Add files via upload

---
+++ config/initializers/pagy.rb
@@ -0,0 +1,12 @@
+require 'pagy/extras/headers'
+require 'pagy/extras/limit'
+require 'pagy/extras/countless'
+require 'pagy/extras/bootstrap'
+require 'pagy/extras/array'
+require 'pagy/extras/overflow'
+require 'pagy/extras/i18n'
+
+Pagy::DEFAULT[:limit] = 100
+Pagy::DEFAULT[:limit_param] = :per_page
+Pagy::DEFAULT[:limit_max] = 1000
+Pagy::DEFAULT[:overflow] = :empty_page

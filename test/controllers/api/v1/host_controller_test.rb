b5b28e53ab59c877f4ccf8c59edd3196c58d7117
Ron Asnahon <asnahonron@gmail.com>
2025-06-28 13:03:43 +0800

    Create host_controller_test.rb

---
+++ test/controllers/api/v1/host_controller_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class ApiV1HostsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @host = Host.find_or_create_by(name: 'GitHub') do |h|
+      h.url = 'https://github.com'
+      h.kind = 'github'
+    end
+    @visible_owner = @host.owners.create!(login: 'visible-owner', kind: :user)
+    @hidden_owner = @host.owners.create!(login: 'hidden-owner', kind: :organization, hidden: true)
+  end
+
+  test 'lists hosts' do
+    get api_v1_hosts_path
+    assert_response :success
+    assert_template 'hosts/index', file: 'hosts/index.json.jbuilder'
+    
+    actual_response = JSON.parse(@response.body)
+
+    assert_equal actual_response.length, 1
+  end
+
+  test 'get a host' do
+    get api_v1_host_path(id: @host.name)
+    assert_response :success
+    assert_template 'hosts/show', file: 'hosts/show.json.jbuilder'
+    
+    actual_response = JSON.parse(@response.body)
+
+    assert_equal actual_response["name"], 'GitHub'
+  end
+
+end
